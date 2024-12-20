fe09215f5e967b365a2f6705251bbeaef1acb49c
Milind Mishra <milind.mishra4@gmail.com>
2024-12-20 21:40:20 +0530

    Refactor next.config.ts to remove environment variable configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
